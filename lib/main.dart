2ffebd0dcc40300777658ca970907342f4bc45a6
santhakumar <santhakumar3490@gmail.com>
2024-01-10 23:06:09 +0530

    android&IOS-PhoneAuth,FireStoreDataSave-and-ImageSaveOnStoageofFirebase-SharedPreference-working_fine_success

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:phoneauth_firebase/provider/auth_provider.dart';
 import 'package:provider/provider.dart';
-
 import 'screens/welcome_screen.dart';
 
 
